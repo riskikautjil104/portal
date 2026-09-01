83ca125f66db14e8bbe679e8b6c593af9ff298a0
Rizki H.I <ibrahimriski211@gmail.com>
2026-09-01 16:45:28 +0900

    .

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\News;
+use App\Services\FcmService;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
